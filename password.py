14ea89fa339fee8dcf807d8cf97cf4d2807750c4
Spade1095 <henrym181@nycstudents.net>
2025-11-19 06:43:06 -0800

    Update password.py

--- password.py
+++ password.py
@@ -3,9 +3,8 @@ def checkIfGood(email,password):
     if not "@" in email: return "Error email does not include '@'. "
     if len(password) < 8: return "Password is less then 8 characters. "
     found = False
-    for i in range(10):
-        if str(i) in password:
-            found = True
+    for i in range(10): 
+        if str(i) in password: found = True
     if not found: return "Password must incude a number"
     if password.islower(): return "Password must have a capital letter. "
     return {"email":email, "password":password}
