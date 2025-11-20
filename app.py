c953fbca3fddb1f0e07b57d7b162ac01f0b7eed6
Spade1095 <henrym181@nycstudents.net>
2025-11-20 07:17:09 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import requests
-
-def getPoke(poke):
+import tkinter
+tk = tkinter
+def getInput():
+    input = inputEntry.get()
+    return input
+window = tk.Tk()
+inputLabel = tk.Label(window,text="What pokemon?")
+inputEntry = tk.Entry(window)
+inputButton = tk.Button(window,text="Submit pokemon",command=getInput)
+inputEntry.grid(row=1,column=2)
+inputLabel.grid(row=1,column=1)
+inputButton.grid(row=1,column=3)
+def getPokeData(poke):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
-    
     data = response.json()
     return {
         "name": data["name"],
@@ -13,6 +23,5 @@ def getPoke(poke):
         "weight": data["weight"],
         "types": [t["type"]["name"] for t in data["types"]]
     }
-
-pokemon = getPoke("Bulbasaur")
-print(pokemon)
+print(getPokeData("Bulbasaur"))
+window.mainloop()
