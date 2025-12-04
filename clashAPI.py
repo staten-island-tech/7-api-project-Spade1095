82ed27547678bad53df464a5236bc4a5d362c33d
Spade1095 <henrym181@nycstudents.net>
2025-12-04 10:17:15 -0500

    Update clashAPI.py

--- clashAPI.py
+++ clashAPI.py
@@ -1,6 +1,7 @@
 import requests
 import tkinter as tk
 from PIL import Image, ImageTk
+from io import BytesIO
 # 165.155.164.16
 token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImQ5YjI1MWIzLTY5M2QtNGVhZS1iZTkxLTI3NmQxZjdlYjcyOSIsImlhdCI6MTc2NDE2ODAzNCwic3ViIjoiZGV2ZWxvcGVyLzIzYzBlM2JhLWQ1YmQtMzU5Zi1hM2NiLTliMzFiZjk0ZDM5YyIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNjUuMTU1LjE2NC4xNiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.9AyyT7PJcwWUVZvYeb6eS_IpCTJOfyM6i49lM0byv80xLb-nRk3ku0UamzikhK6L_PTCG9GDJtvPpVk0IoWUtA"
 # 165.155.163.5
@@ -12,7 +13,6 @@ def getInput():
     input = inputEntry.get()
     return input
 def getCardData():
-    found = False
     name = getInput()
     print(f"Name : {name}")
     response = requests.get("https://api.clashroyale.com/v1" + endpoint, api_key)
@@ -24,16 +24,15 @@ def getCardData():
     for i in range(len(data["items"])):
         try:
             if data["items"][i]["name"] == name:
-                found = True
                 data = data["items"][i]
-                print("test")
                 nameLabel = tk.Label(window, text=f"Name: {data["name"]}").grid(column=2,row=2)
                 elixirLabel = tk.Label(window, text=f"Elixir Cost: {data["elixirCost"]}").grid(column=2,row=3)
                 rarityLabel = tk.Label(window, text=f"Rarity: {data["rarity"]}").grid(column=2,row=4)
-                baseCardImage = Image.open(data[])
-                print(data)
+                imageResponse = requests.get(data["iconUrls"]["medium"])
+                print(BytesIO(imageResponse.content))
+                baseCardImage = Image.open(BytesIO(imageResponse.content))
+                baseCardImage.show()
             elif i == len(data["items"]) - 1:
-                found = False
                 print("card not found")
         except KeyError:
             continue
