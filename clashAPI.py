f64e03d188da7837fe6d6ab64ba1762b6e5d97e9
Spade1095 <henrym181@nycstudents.net>
2025-12-11 10:17:21 -0500

    Update clashAPI.py

--- clashAPI.py
+++ clashAPI.py
@@ -26,26 +26,32 @@ def getCardData():
     token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImQ5YjI1MWIzLTY5M2QtNGVhZS1iZTkxLTI3NmQxZjdlYjcyOSIsImlhdCI6MTc2NDE2ODAzNCwic3ViIjoiZGV2ZWxvcGVyLzIzYzBlM2JhLWQ1YmQtMzU5Zi1hM2NiLTliMzFiZjk0ZDM5YyIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNjUuMTU1LjE2NC4xNiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.9AyyT7PJcwWUVZvYeb6eS_IpCTJOfyM6i49lM0byv80xLb-nRk3ku0UamzikhK6L_PTCG9GDJtvPpVk0IoWUtA"
     api_key = {"Authorization": f"Bearer {token}"}
     response = requests.get("https://api.clashroyale.com/v1" + endpoint, api_key) 
-    if response.status_code != 200 and response.status_code!= 403:
+    if response.status_code != 200 and response.status_code != 403:
         print("Error fetching data!")
         print(response)
         print(response.json())
-        return None
+        # return None
     if response.status_code == 403:
-        token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhlZTU1MTg0LTg4MzQtNDI5ZC1iZTE5LTI3ODhhNWM2MjM0NyIsImlhdCI6MTc2NDE2ODEwMiwic3ViIjoiZGV2ZWxvcGVyLzIzYzBlM2JhLWQ1YmQtMzU5Zi1hM2NiLTliMzFiZjk0ZDM5YyIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNjUuMTU1LjE2My41Il0sInR5cGUiOiJjbGllbnQifV19.qDGz1KhmbidyL0HsNEmoipsXDOFrZJhetbfOYlDb7sI9TW6VUsvkMAHJOMOIR8tRxRW0kq8VJEMxgtvyhxUibA"
+        token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjExMDBiMGJkLTViODItNDQ5Yi05ZGZmLWZiNzYwNDg5MmI3OSIsImlhdCI6MTc2NTQ2NTM1NCwic3ViIjoiZGV2ZWxvcGVyLzIzYzBlM2JhLWQ1YmQtMzU5Zi1hM2NiLTliMzFiZjk0ZDM5YyIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNjUuMTU1LjE3Mi42Il0sInR5cGUiOiJjbGllbnQifV19.MVhP7qf_aF35xsXoANGc5lMo3prBPaHmD-IB0-aKkSGdP2rTiT0nGrWG3Rm6OukOvBkZFagwPJLnWbfnuk9B0g"
         api_key = {"Authorization": f"Bearer {token}"}
         response = requests.get("https://api.clashroyale.com/v1" + endpoint, api_key)
+        print("IP number 2")
     
     data = response.json()
-    # deleate previus labels
-    if len(tks) > 0:
-        for i in range(len(tks)):
-            tks[i].destroy()
-    # find data 
+
+    # find data
+    try:
+        test = data["items"]
+    except KeyError:
+        print('items doesnt exsit')
     for i in range(len(data["items"])):
         try:
             if data["items"][i]["name"] == name:
                 data = data["items"][i]
+                # deleate previus labels
+                if len(tks) > 0:
+                    for i in range(len(tks)):
+                        tks[i].destroy()
                 # make labels
                 nameLabel = tk.Label(window, text=f"Name: {data["name"]}")
                 elixirLabel = tk.Label(window, text=f"Elixir Cost: {data["elixirCost"]}")
